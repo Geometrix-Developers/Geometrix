48629978ebbf73795186592a28faa4b1fed6b6a0
Bykov Platon <103729245+foxycoderr@users.noreply.github.com>
2023-02-03 22:26:37 +0200

    Created GeometrixWorkfield class in master.py

--- Geometrix/master.py
+++ Geometrix/master.py
@@ -1,40 +1,40 @@
-from Geometrix.line import Line
 from Geometrix.point import Point
 
-POINTS = []
-LINES = []
 
+class GeometrixWorkfield:
+    def __init__(self):
+        self.points = []
+        self.lines = []
 
-def add_point(x, y):
-    """
-    add_point function - adds a point
-    - requires coordinates
-    - return point ID
-    """
+    def add_point(self, x, y):
+        """
+        add_point function - adds a point
+        - requires coordinates
+        - return point ID
+        """
 
-    point = Point(x, y, len(POINTS))
-    POINTS.append(point)
-    return f"Created point with ID: {point.id}"
+        point = Point(x, y, len(self.points))
+        self.points.append(point)
+        return f"Created point with ID: {point.id}"
 
+    def add_line(self, point1_index, point2_index):
+        """
+        add_line function - connects two points
+        - requires points IDs
+        - returns line ID
+        """
 
-def add_line(point1_index, point2_index):
-    """
-    add_line function - connects two points
-    - requires points IDs
-    - returns line ID
-    """
+        try:
+            point1 = self.points[point1_index]
+            point2 = self.points[point2_index]
+        except:
+            raise RuntimeError("Point not found")
 
-    try:
-        point1 = POINTS[point1_index]
-        point2 = POINTS[point2_index]
-    except:
-        raise RuntimeError("Point not found")
+        self.points[point1_index].connect(point2)
+        self.points[point2_index].connect(point1)
 
-    POINTS[point1_index].connect(point2)
-    POINTS[point2_index].connect(point1)
+        line = Line(point1, point2, len(self.lines))
+        self.lines.append(line)
 
-    line = Line(point1, point2, len(LINES))
-    LINES.append(line)
-
-    return f"Created line with ID: {line.id}"
+        return f"Created line with ID: {line.id}"
 
