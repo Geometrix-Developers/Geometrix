82f5fa6d5ef6fb7163ceaf867c137fa97e40bc14
Bykov Platon <103729245+foxycoderr@users.noreply.github.com>
2023-02-05 14:30:10 +0200

    Updated development plans, started work on new plans

--- Geometrix/__init__.py
+++ Geometrix/__init__.py
@@ -11,7 +11,7 @@ class GeometrixWorkfield:
         """
         add_point function - adds a point
         - requires coordinates
-        - return point ID
+        - return point object
         """
 
         point = Point(x, y, len(self.points))
@@ -23,7 +23,7 @@ class GeometrixWorkfield:
         """
         add_line function - connects two points
         - requires points IDs
-        - returns line ID
+        - returns line object
         """
 
         try:
@@ -39,4 +39,80 @@ class GeometrixWorkfield:
         self.lines.append(line)
 
         return line
+
+    def add_triangle(self, p1_id, p2_id, p3_id):
+        """
+        add_triangle function - connects three points
+        - requires IDs of points
+        - returns list of three line objects
+        """
+
+        for line in self.lines:
+            if [p1_id, p2_id] in [line.point_1.id, line.point_2.id] or [p1_id, p3_id] in [line.point_1.id, line.point_2.id] or [p3_id, p2_id] in [line.point_1.id, line.point_2.id]:
+                raise RuntimeError("One or more lines of the triangle appear to already present.")
+
+        try:
+            point1 = self.points[p1_id]
+            point2 = self.points[p2_id]
+            point3 = self.points[p3_id]
+        except:
+            raise RuntimeError("Point not found")
+
+        self.points[p1_id].connect(point2)
+        self.points[p1_id].connect(point3)
+        self.points[p2_id].connect(point1)
+        self.points[p2_id].connect(point3)
+        self.points[p3_id].connect(point1)
+        self.points[p3_id].connect(point2)
+
+        line1 = Line(point1, point2, len(self.lines))
+        self.lines.append(line1)
+        line2 = Line(point2, point3, len(self.lines))
+        self.lines.append(line2)
+        line3 = Line(point1, point3, len(self.lines))
+        self.lines.append(line3)
+
+        return [line1, line2, line3]
+
+    def add_quadrilateral(self, p1_id, p2_id, p3_id, p4_id):
+        """
+        add_triangle function - connects three points
+        - requires IDs of points
+        - returns list of three line objects
+        """
+
+        point_ids = [p1_id, p2_id, p3_id, p4_id]
+        for line in self.lines:
+            for point_id in point_ids:
+                for linepoint_id in [line.point_1.id, line.point_2.id]:
+                    if point_id == linepoint_id:
+                        raise RuntimeError("One or more lines of the quadrilateral appear to already present.")
+
+        try:
+            point1 = self.points[p1_id]
+            point2 = self.points[p2_id]
+            point3 = self.points[p3_id]
+            point4 = self.points[p4_id]
+            points = [point1, point2, point3, point4]
+        except:
+            raise RuntimeError("Point not found")
+
+        """
+        for point1_id in point_ids:
+            for point2_id in point_ids:
+                if not point1_id == point2_id:
+                    self.points[point1_id].connect(point2_id)
+
+        return_list = []
+        for point1_obj in points:
+            for point2_obj in points:
+                if point2_obj != point1_obj:
+                    line = Line(point1, point2, len(self.lines))
+                    self.lines.append(line)
+                    return_list.append(line) 
+
+        return return_list 
+        """
+
+        return None # for now, due to lack of algorithm of line creation
 
