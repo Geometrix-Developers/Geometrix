0da1a40a110eaf3efdabc2137d7d19cf31fb3252
Bykov Platon <103729245+foxycoderr@users.noreply.github.com>
2023-02-07 21:55:34 +0200

    Added negative coordinates, removed point number limit.

--- Geometrix/point.py
+++ Geometrix/point.py
@@ -12,18 +12,10 @@ class Point:
     """
 
     def __init__(self, x, y, id):
-        if x < 0 or y < 0: # makes sure point is on work field
-            raise RuntimeError("Point cannot be outside work field")
-        else:
-            self.x = x
-            self.y = y
-
+        self.x = x
+        self.y = y
         self.connected_to = [] # list of connected points
-
-        if id > 1000: # limits number of points to 1000
-            raise RuntimeError("No more than 1000 points can be created")
-        else:
-            self.id = id
+        self.id = id
 
     def connect(self, point:Point):
         self.connected_to.append(point)
